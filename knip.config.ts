240c646d62a47f3d63e18565633ca58f948bfbce
Florian Wessels <flo.wessels@gmail.com>
2026-03-22 09:57:58 +0100

    [TASK] Rename route group

--- knip.config.ts
+++ knip.config.ts
@@ -15,7 +15,7 @@ const config: KnipConfig = {
       'src/proxy.ts'
     ]
   },
-  ignore: ['src/lib/db.ts', 'src/app/(dashbaord)/actions/revalidateArticles.ts'],
+  ignore: ['src/lib/db.ts', 'src/app/(dashboard)/actions/revalidateArticles.ts'],
   ignoreDependencies: ['postcss']
 }
 
